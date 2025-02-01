d057b132da3683d063111de7491effda40756a6b
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2025-02-01 23:39:55 +0530

    updTE

--- Day10/remainingHooks/src/App.jsx
+++ Day10/remainingHooks/src/App.jsx
@@ -1,33 +1,32 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useEffect } from 'react';
 
+
+//Fibonacci Number: 0 1 1 2 3 5 8 13 21
 function App() {
-  const [count, setCount] = useState(0)
-
+  let [count,setCount]=useState(0);
+  const [number,setNumber]=useState("")
+  const [result,setResult]=useState(0)
+  const [clicked,setClicked]=useState(false);
+  function Fibonacci(n){
+    if(n<=1) return 1;
+    return Fibonacci(n-1)+Fibonacci(n-2);
+  }
+  useEffect(()=>{
+    setResult(Fibonacci(Number(number)))
+  },[clicked])
   return (
+
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1>Count is:{count}</h1>
+      <button onClick={()=>setCount(count+1)}>Increment</button>
+
+      <div className='forms'>
+        <h2>Fib ans is:{result}</h2>
+        <input type='text' onChange={()=>setNumber(event.target.value)} value={number}></input>
+        <button onClick={()=>setClicked(!clicked)}>Show Fib</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
