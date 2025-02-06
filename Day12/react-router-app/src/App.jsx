8237acd0c00eb5dee640e6ad0979e3d3e7343fcd
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2025-02-06 12:12:08 +0530

    nested routing

--- Day12/react-router-app/src/App.jsx
+++ Day12/react-router-app/src/App.jsx
@@ -2,7 +2,10 @@ import {BrowserRouter,Routes, Route,Link} from "react-router-dom"
 import Home from "./Home"
 import Contact from "./Contact"
 import Dashboard from "./Dashboard"
-
+import Details from "./Details"
+import Zero from "./Zero"
+import Hello from "./Hello"
+import Hi from "./Hi"
 function App() {
 
   return (
@@ -11,11 +14,17 @@ function App() {
       <Link to="/">Home</Link>
       <Link to="/contact">Contact</Link>
       <Link to="/dashboard">dashboard</Link>
+      <Link to="/details">Details</Link>
     </nav>
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/contact" element={<Contact />}></Route>
         <Route path="/dashboard" element={<Dashboard />}></Route>
+        <Route path="/details" element={<Details />}>
+          <Route index element={<Zero />}></Route>
+          <Route path="Hello" element={<Hello />}></Route>
+          <Route path="Hi" element={<Hi />}></Route>
+        </Route>
       </Routes>
     </BrowserRouter>
   )
