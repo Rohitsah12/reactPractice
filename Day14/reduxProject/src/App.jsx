25daae732e9982272a8a9722889c9a0af193899b
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2025-02-11 17:22:39 +0530

    build counter using redux

--- Day14/reduxProject/src/App.jsx
+++ Day14/reduxProject/src/App.jsx
@@ -1,3 +1,11 @@
+import { Provider } from "react-redux";
+import Counting from "./Counting";
+import stores from "./Stores";
+
 export default function App(){
-  
+  return(
+    <Provider store={stores}>
+        <Counting />
+    </Provider>
+  )
 }
