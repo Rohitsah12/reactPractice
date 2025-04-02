25c988f90e706e6a13577ca00973a5112b409e08
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2025-04-02 15:39:35 +0530

    Added movie card ui

--- movieSearchApp/src/pages/Home.jsx
+++ movieSearchApp/src/pages/Home.jsx
@@ -1,10 +1,27 @@
+import MovieCard from "../components/Navbar/MovieCard/MovieCard";
+import './Home.css'
 function Home(){
+    const movie={
+        "Title":"Harry Potter and the Deathly Hallows: Part 2",
+        "Year":"2011",
+        "imdbID":"tt1201607",
+        "Type":"movie",
+        "Poster":"https://m.media-amazon.com/images/M/MV5BOTA1Mzc2N2ItZWRiNS00MjQzLTlmZDQtMjU0NmY1YWRkMGQ4XkEyXkFqcGc@._V1_SX300.jpg"
+      }
     return(
         <>
-            {/* navbar */}
-            {/* movie list */}
-            {/* pagination buttons */}
-            Home
+            <div className="movie-card-wrapper">
+            <MovieCard
+                {...movie}
+             />
+             <MovieCard
+                {...movie}
+             />
+             <MovieCard
+                {...movie}
+             />
+            </div>
+            
         </>
     )
 }
